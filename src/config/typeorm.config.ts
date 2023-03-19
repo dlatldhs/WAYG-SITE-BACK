c12f1a0f750206ba87052cd97f07ef758fe2fb95
dlatldhs <sionzz1713@gmail.com>
2023-03-19 19:37:52 +0900

    Fix: mysql connection

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -5,11 +5,11 @@ import * as dotenv from "dotenv";
 dotenv.config()
 export const typeORMConfig : TypeOrmModuleOptions = {
     type: 'mysql',
-    host: 'localhost',
-    port: 3306,
+    host: process.env.HOST,
+    port: 31779,//3306,
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'wayg',
+    database: process.env.DB_NAME,
     entities: [__dirname + '/../**/*.entity.{js,ts}',School],
     synchronize: true
 }
